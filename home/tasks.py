b32ae889b6798fdb162c9f428a1ad20667f6953c
Bibek Khatri <bibekkhatri291@gmail.com>
2024-08-29 10:19:34 +1000

    Fixes

--- home/tasks.py
+++ home/tasks.py
@@ -1,6 +1,8 @@
 from celery import shared_task
+from time import sleep
 
 
 @shared_task(bind=True, ignore_result=True)
 def debug_task(self):
+    sleep(20)
     print(f"Request: {self.request!r}")
